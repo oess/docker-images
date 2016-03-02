5999e619758797cc7aa1c8d32f5e19a1dce60138
Matt Geballe <matt@geballe.net>
2016-03-02 13:25:36 -0700

    Storing notebooks in subfolder of HOME

--- oenb_tmpnb_worker/start_jupyter.bash
+++ oenb_tmpnb_worker/start_jupyter.bash
@@ -6,10 +6,12 @@ ${CONDADIR}/bin/ipython kernel install --user
 source activate ${CONDADIR}
 ipython kernel install
 
-cp -R /hub_data/notebooks/* ./
+mkdir -p $HOME/notebooks
+
+cp -R /hub_data/notebooks/* $HOME/notebooks/
 
 chown -R oe-user:oe-user /home/oe-user
 
-RUN find . -name '*.ipynb' -exec ipython2 trust {} \;
+RUN find $HOME/notebooks -name '*.ipynb' -exec ipython2 trust {} \;
 
-jupyter-notebook --no-browser --NotebookApp.ip='*' $*
+jupyter-notebook --no-browser --NotebookApp.ip='*' --notebook-dir=$HOME/notebooks $*
